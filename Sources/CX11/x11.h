38bee660e4a62e7b9036fc9b9fc2ffaf96a4d30b
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-08-12 19:39:23 +0530

    [dep] Update X11 libs

--- Sources/CX11/x11.h
+++ Sources/CX11/x11.h
@@ -3,8 +3,11 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xinerama.h>
+#include <X11/extensions/Xrandr.h>
 #include <X11/XF86keysym.h>
 #include <X11/keysym.h>
 #include <X11/XKBlib.h>
 #include <X11/Xatom.h>
 #include <X11/Xutil.h>
+#include <X11/cursorfont.h>
+#include <X11/Xproto.h>
