35a1b33f67f845676df1a4e8acab2aae34c049e3
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-08-12 19:38:40 +0530

    [dep] Add swift-log

--- Package.swift
+++ Package.swift
@@ -4,13 +4,21 @@ import PackageDescription
 
 let package = Package(
     name: "SWM",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    ],
     targets: [
         .executableTarget(
             name: "swm",
-            dependencies: ["CX11"],
-            path: "Sources"),
+            dependencies: [
+                "CX11",
+                .product(name: "Logging", package: "swift-log"),
+            ],
+            path: "Sources"
+        ),
         .systemLibrary(
             name: "CX11",
-            pkgConfig: "x11"),
+            pkgConfig: "x11"
+        ),
     ]
 )
